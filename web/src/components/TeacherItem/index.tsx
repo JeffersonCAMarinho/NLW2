3ea7a847c473751d6fdcb4e1de43a65b44bc8e8e
Jefferson Marinho <jeffersonmarinho97@gmail.com>
2020-08-09 19:31:08 -0300

    NLW 02

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,56 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem(){
+
+
+export interface Teacher {
+
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps{
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+  function createNewConnection(){
+    api.post('connections',{
+      user_id: teacher.id,
+    })
+  }
+  
   return (
     <article className="teacher-item">
             <header>
-              <img src="https://avatars2.githubusercontent.com/u/32003507?s=460&u=3fabc06856a4e89cbefc7bb14510bb709210cfa8&v=4" alt="Jefferson Marinho"/>
+              <img src={teacher.avatar} alt={teacher.name}/>
               <div>
-                <strong>Jefferson Marinho</strong>
-                <span>Manutencao de Computadores</span>
+                <strong>{teacher.name}</strong>
+                <span>{teacher.subject}</span>
 
               </div>
             </header>
             <p>
-              Estudante de computacao precisando de jobs
-              <br/><br/>
-            Busco ensinar as pessoas como fazer a manutencao de seus computadores pessoais e 
-            se tornar o sobrinho do TI que formata PC de graca pra familia
+              {teacher.bio}
             </p>
             <footer>
               <p>
                 Preco/Hora
-                <strong>R$ 150,00</strong>
+                <strong>R$ {teacher.cost}</strong>
               </p>
-              <button type="button">
-                <img src={whatsappIcon} alt="Whatsapp"/>
+              <a 
+              target="_blank"
+              onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
+              <img src={whatsappIcon} alt="Whatsapp"/>
                 Entrar em contato
-              </button>
+              </a>
             </footer>
           </article>
   )
